3a5a220c8717f84bae4aaeee45e0c8b6d4c6aaa8
DakshRocks21 <85815311+DakshRocks21@users.noreply.github.com>
2025-01-15 11:57:07 +0800

    Added outline of Airline;BoardingGate;Terminal; Update .gitignore to exclude IDE and build files

--- BoardingGate.cs
+++ BoardingGate.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace S10266136_PRG2Assignment
 {
-    internal class BoardingGate
+    class BoardingGate
     {
+        public string GateName {  get; set; }
+        public bool SupportsCFFT { get; set; }
+        public bool SupportsDDJB { get; set; }
+        public bool SupportsLWTT { get; set; }
+        public Flight Flight { get; set; }
+
+        public double CalculateFees()
+        {
+            return 0.0;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
     }
 }
--- Terminal.cs
+++ Terminal.cs
@@ -6,7 +6,45 @@ using System.Threading.Tasks;
 
 namespace S10266136_PRG2Assignment
 {
-    internal class Terminal
+    class Terminal
     {
+        public string TerminalName { get; set; }
+        public Dictionary<string, Airline> Airlines { get; set; }
+
+        public Dictionary<string, Flight> Flights { get; set; }
+
+        public Dictionary<string, BoardingGate> BoardingGates { get; set; }
+
+        public Dictionary<string, Double> GateFee { get; set; }
+
+        
+        public bool AddAirline(Airline airline)
+        {
+            return true;
+        }
+
+        public bool AddBoardingGate(BoardingGate boardingGate)
+        {
+            return true;
+        }
+
+        public Airline GetAirlineFromFlight(Flight flight)
+        {
+            return new Airline();
+
+        }
+
+        public void PrintAirlineFees()
+        {
+            return;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+
+    }
     }
 }
