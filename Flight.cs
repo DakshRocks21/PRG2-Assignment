af65a513f1f497f7ea5555a470cddef268cb8f22
DakshRocks21 <85815311+DakshRocks21@users.noreply.github.com>
2025-01-15 12:37:49 +0800

    Added theres's flight.cs

--- Flight.cs
+++ Flight.cs
@@ -6,14 +6,36 @@ using System.Threading.Tasks;
 
 namespace S10266136_PRG2Assignment
 {
-    class Flight
+    abstract class Flight
     {
 
-        public string flightNumber { get; set; }
+        public string FlightNumber { get; set; }
+        public string Origin { get; set; }
+        public string Destination { get; set; }
+        public DateTime ExpectedTime { get; set; }
+        public string Status { get; set; }
 
-        public string flightNumber { get; set; }
-        public string flightNumber { get; set; }
+        public Flight(string flightNumber, string origin, string destination, DateTime expectedTime, string status)
+        {
+            FlightNumber = flightNumber;
+            Origin = origin;
+            Destination = destination;
+            ExpectedTime = expectedTime;
+            Status = status;
+        }
 
+        public double CalculateFees()
+        {
+            // Calculate the fees
+            return;
+        }
+
+
+
+        public override string ToString()
+        {
+            return $"Flight Number: {FlightNumber}\nOrigin: {Origin}\nDestination: {Destination}\nExpected Time: {ExpectedTime}\nStatus: {Status}";
+        }
 
     }
 }
